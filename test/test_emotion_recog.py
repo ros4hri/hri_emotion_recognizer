2b5e16faca3273c0965929967feea50433d82737
Séverin Lemaignan <severin.lemaignan@pal-robotics.com>
2024-09-18 17:44:34 +0200

    rewrote the unit-test to use simpler logic and faster sim time

--- test/test_emotion_recog.py
+++ test/test_emotion_recog.py
@@ -16,26 +16,20 @@ import rclpy
 from rclpy.executors import SingleThreadedExecutor, TimeoutException
 from sensor_msgs.msg import Image
 from rosgraph_msgs.msg import Clock
-from hri_msgs.msg import Expression
 from datetime import timedelta, datetime
 from rclpy.time import Time
+from rclpy.duration import Duration
+from rclpy.parameter import Parameter
 import unittest
 from cv_bridge import CvBridge
 import cv2
-import time
 import csv
 from hri import HRIListener
 from hri_emotion_recognizer.hri_emotion_recognizer import NodeEmotionRecognizer
 from hri_face_detect.node_face_detect import NodeFaceDetect
 from pathlib import Path
 
-
-import numpy as np
-import random
-
-# Set seeds for reproducibility
-np.random.seed(42)
-random.seed(42)
+NODE_RATE = 10.
 
 
 def spin_some(executor, timeout=timedelta(seconds=10.)):
@@ -61,35 +55,57 @@ class TestHRIEmotionRecognizer(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         rclpy.init()
-        cls.emotion_recognizer_node = NodeEmotionRecognizer()
+
         cls.face_detect_node = NodeFaceDetect()
+        cls.face_detect_node.set_parameters([
+            Parameter('deterministic_ids', Parameter.Type.BOOL, True),
+            Parameter('use_sim_time', Parameter.Type.BOOL, True)])
+
+        cls.emotion_recognizer_node = NodeEmotionRecognizer()
+        cls.emotion_recognizer_node.set_parameters([
+            Parameter('use_sim_time', Parameter.Type.BOOL, True)])
+
         cls.emotion_executor = SingleThreadedExecutor()
+
         cls.emotion_executor.add_node(cls.emotion_recognizer_node)
         cls.emotion_executor.add_node(cls.face_detect_node)
+
         cls.face_detect_node.trigger_configure()
         cls.emotion_recognizer_node.trigger_configure()
+
         return super().setUpClass()
 
     @classmethod
     def tearDownClass(cls):
         cls.emotion_recognizer_node.destroy_node()
         cls.face_detect_node.destroy_node()
+        cls.emotion_executor.shutdown()
         rclpy.shutdown()
         return super().tearDownClass()
 
     def setUp(self) -> None:
         self.tester_node = rclpy.create_node('tester_node')
+
+        self.time = Time()
+
         self.clock_pub = self.tester_node.create_publisher(Clock, '/clock', 1)
         self.bridge = CvBridge()
         self.image_publisher = self.tester_node.create_publisher(
             Image, '/image', 10)
+
         self.subscriptions = {}
+
         self.expression_received = {}
+
         self.tester_executor = SingleThreadedExecutor()
         self.tester_executor.add_node(self.tester_node)
-        self.hri_listener = HRIListener('tester_emotion_listener', False)
+
+        self.hri_listener = HRIListener(
+            'tester_emotion_listener', False, use_sim_time=True)
+
         self.face_detect_node.trigger_activate()
         self.emotion_recognizer_node.trigger_activate()
+
         return super().setUp()
 
     def tearDown(self) -> None:
@@ -110,80 +126,81 @@ class TestHRIEmotionRecognizer(unittest.TestCase):
                 emotions[image_filename] = emotion
         return emotions
 
-    def publish_image_callback(self):
-        if hasattr(self, 'current_image'):
-            img = cv2.imread(str(self.current_image))
-            img_msg = self.bridge.cv2_to_imgmsg(img, encoding='bgr8')
-            self.image_publisher.publish(img_msg)
+    def fastforward_time(self):
+        self.time = self.time + Duration(seconds=(1/NODE_RATE))
+        self.clock_pub.publish(Clock(clock=self.time.to_msg()))
 
-    def spin(self, time_ns=None):
-        if time_ns is not None:
-            self.clock_pub.publish(
-                Clock(clock=Time(nanoseconds=time_ns).to_msg()))
-        spin_some(self.emotion_executor)
-        self.hri_listener.spin_some(timedelta(seconds=5.))
+    def spin(self):
+
+        # publish the image
         spin_some(self.tester_executor)
 
-    def expression_callback(self, face_id):
-        def callback(msg):
-            self.expression_received[face_id] = msg
-        return callback
+        self.fastforward_time()
+
+        # hri_face_detect detects the face
+        spin_some(self.emotion_executor)
+        spin_some(self.emotion_executor)
+
+        # libhri publish the detected faces
+        self.hri_listener.spin_some(timedelta(seconds=5.))
+
+        self.fastforward_time()
+
+        # hri_emotion_recognizer recognizes the emotion
+        spin_some(self.emotion_executor)
+        # libhri publish the recognized emotions
+        self.hri_listener.spin_some(timedelta(seconds=5.))
 
     def test_emotion_recognition(self):
         # If running as python3 test_emotion_recog.py
         # csv_file_path = Path().cwd() / 'data'
         # If running as colcon test-result --verbose
         csv_file_path = Path().cwd() / 'test' / 'data'
-        expected_expression_mapping = {
-            'ANGRY': Expression.ANGRY,
-            'HAPPY': Expression.HAPPY,
-            'SURPRISED': Expression.SURPRISED,
-            'SAD': Expression.SAD,
-            'DISGUSTED': Expression.DISGUSTED,
-            'NEUTRAL': Expression.NEUTRAL,
-            'SCARED': Expression.SCARED,
-        }
         self.expected_emotions = self.load_expected_emotions(csv_file_path)
 
+        self.spin()
+
         for img_path, expected_emotion in self.expected_emotions.items():
-            found = False
             self.current_image = csv_file_path / img_path
-            expected_emotion_enum = expected_expression_mapping[expected_emotion]
 
-            # Start publishing the image
-            self.image_timer = self.tester_node.create_timer(
-                0.1, self.publish_image_callback)
+            print(
+                f"Testing image {self.current_image} -> expected emotion {expected_emotion}")
 
-            # Allow some time for the emotion recognizer to process
-            timeout = 10  # seconds
-            start_time = time.time()
-            found = False
+            img = cv2.imread(str(self.current_image))
+            img_msg = self.bridge.cv2_to_imgmsg(img, encoding='bgr8')
 
-            while time.time() - start_time < timeout:
-                self.spin()
-                # Dynamically create subscriptions for detected face IDs
-                for face_id in list(self.hri_listener.faces.keys()):
-                    if face_id not in self.subscriptions:
-                        self.subscriptions[face_id] = self.tester_node.create_subscription(
-                            Expression,
-                            f'/humans/faces/{face_id}/expression',
-                            self.expression_callback(face_id),
-                            10
-                        )
+            # need to publish the image multiple times to get the face detected
+            # due to hri_face_detect tracking algorithm + the generation of the
+            # cropped image. Then, need to spin several times to get all the messages
+            # to the emotion recognizer amd back to libhri
+            self.image_publisher.publish(img_msg)
+            self.spin()
+            self.image_publisher.publish(img_msg)
+            self.spin()
+            self.image_publisher.publish(img_msg)
+            self.spin()
+            self.image_publisher.publish(img_msg)
+            self.spin()
+            self.image_publisher.publish(img_msg)
+            self.spin()
+            self.spin()
+            self.spin()
+            self.spin()
 
-                # Check if the expected emotion has been received
-                for face_id, expression_msg in self.expression_received.items():
-                    print(self.current_image)
-                    print(expression_msg)
-                    print(expected_emotion_enum)
-                    if expression_msg.expression == expected_emotion_enum:
-                        found = True
-                        break
-                if found:
-                    break
+            faces = self.hri_listener.faces
 
             self.assertTrue(
-                found, f"Expected emotion '{expected_emotion}' not found for image '{img_path}'")
+                len(faces) != 0, "No faces detected!")
+
+            self.assertTrue(
+                len(faces) == 1, "Only one face should be detected")
+
+            self.assertIsNotNone(
+                faces[list(faces.keys())[0]].expression, "No expression detected!")
+
+            self.assertEqual(
+                faces[list(faces.keys())[0]].expression.name, expected_emotion,
+                f"Wrong expression detected for {self.current_image}!")
 
 
 if __name__ == '__main__':
